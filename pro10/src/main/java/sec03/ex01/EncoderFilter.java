8c09a579cfd30ff0281a7820ec556c8e9215f73d
huni6711 <huni6711@naver.com>
2025-12-11 17:10:51 +0900

    서블릿 pro10 추가

---
+++ pro10/src/main/java/sec03/ex01/EncoderFilter.java
@@ -0,0 +1,51 @@
+package sec03.ex01;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+
+/**
+ * Servlet Filter implementation class EncoderFilter
+ */
+@WebFilter("/EncoderFilter")
+public class EncoderFilter extends HttpFilter implements Filter {
+       
+    /**
+     * @see HttpFilter#HttpFilter()
+     */
+    public EncoderFilter() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		request.setCharacterEncoding("utf-8");
+		// 가기전에 처리할 것들
+		chain.doFilter(request, response); // 원래 가려던 곳
+		// 돌아갈 떄 처리할 것들
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
