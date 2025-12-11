ff470b1a259affde4100737a093d45492074e39e
huni6711 <huni6711@naver.com>
2025-12-11 17:09:46 +0900

    서블릿 pro9 추가

---
+++ pro09/src/main/java/sec03/ex01/SessionTest2.java
@@ -0,0 +1,43 @@
+package sec03.ex01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class SessionTest
+ */
+@WebServlet("/SessionTest2")
+public class SessionTest2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		HttpSession session = request.getSession();
+		out.println("세션 아이디: " + session.getId()+"<br>");
+		out.println("최초 세션 생성 시각: "+new Date(session.getCreationTime())+"<br>");
+		out.println("최초 세션 접근 시각: "+new Date(session.getLastAccessedTime())+"<br>");
+		out.println("세션 유효 시간: "+ session.getMaxInactiveInterval()+"<br>");
+		if(session.isNew()) {
+			out.print("새 세션이 만들어졌습니다.");
+		}
+		
+		// 값 저장
+		Map<String, String> member = new HashMap<>();
+		member.put("id", "hong");
+		member.put("name", "홍길동");
+		session.setAttribute("member", member);
+	}
+}
---
+++ pro09/src/main/java/sec05/ex01/MemberVO.java
@@ -0,0 +1,14 @@
+package sec05.ex01;
+
+import java.sql.Date;
+
+import lombok.Data;
+
+@Data
+public class MemberVO {
+	private String id;
+	private String pwd;
+	private String name;
+	private String email;
+	private Date joinDate;
+}
